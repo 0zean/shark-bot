ec1d5054bdfe61c806de4c032e49486b300397bf
0zean <pd1138@protonmail.com>
2024-11-10 04:28:22 +0000

    ♻️ Project refactor

---
+++ utils/helper.py
@@ -0,0 +1,25 @@
+import json
+import os
+from urllib.parse import urlparse
+
+
+def load_config():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(script_dir, "configs.json")
+
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    return {
+        "LOCAL_FFMPEG_OPTIONS": config["ffmpeg"]["local"],
+        "STREAM_FFMPEG_OPTIONS": config["ffmpeg"]["stream"],
+        "YDL_OPTIONS": config["ydl"],
+        "AUDIO_TYPES": tuple(config["audio_types"]),
+    }
+
+
+def get_file_extension(url):
+    parsed_url = urlparse(url)
+    file_name = os.path.basename(parsed_url.path)
+    base_name, file_extension = os.path.splitext(file_name)
+    return base_name, file_extension.lower()
