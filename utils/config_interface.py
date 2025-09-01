88ef5546b803163179a3b070e371bb7287f839f6
0zean <pd1138@protonmail.com>
2025-09-01 02:46:43 -0400

    Fix config type

--- utils/config_interface.py
+++ utils/config_interface.py
@@ -1,10 +1,12 @@
 from abc import ABC
 
+from schemas.audio_model import YtDLP
+
 
 class ConfigInterface(ABC):
     LOCAL_FFMPEG_OPTIONS: dict[str, str]
     STREAM_FFMPEG_OPTIONS: dict[str, str]
-    YDL_OPTIONS: dict[str, str]
+    YDL_OPTIONS: YtDLP
     AUDIO_TYPES: tuple[str, ...]
     DELETE_TIMER: int
     INACTIVITY_CHECK: int
